f7b4c63e08aa3e4ed68d73dba5dd649304338489
piupiujk <docs4p@yandex.ru>
2023-06-06 19:10:22 +0500

    added stripe_product_price_id to Product model

--- orders/views.py
+++ orders/views.py
@@ -11,6 +11,7 @@ from django.views.decorators.csrf import csrf_exempt
 
 from common.views import TitleMixin
 from orders.forms import OrderForm
+from products.models import Basket
 
 stripe.api_key = settings.STRIPE_SECRET_KEY
 
@@ -32,13 +33,9 @@ class OrderCreateView(TitleMixin, CreateView):
 
     def post(self, request, *args, **kwargs):
         super(OrderCreateView, self).post(request, *args, **kwargs)
+        baskets = Basket.objects.filter(user=self.request.user)
         checkout_session = stripe.checkout.Session.create(
-            line_items=[
-                {
-                    'price': 'price_1NDE2gA4w5vEYR5cL4mmijn4',
-                    'quantity': 1,
-                },
-            ],
+            line_items=baskets.stripe_products(),
             metadata={'order_id': self.object.id},
             mode='payment',
             success_url='{}{}'.format(settings.DOMAIN_NAME, reverse('orders:order_success')),
@@ -71,15 +68,20 @@ def stripe_webhook_view(request):
     # Handle the checkout.session.completed event
     if event['type'] == 'checkout.session.completed':
         # Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
-        session = event['data']['object']
+        session = stripe.checkout.Session.retrieve(
+            event['data']['object']['id'],
+            expand=['line_items'],
+        )
 
-        fulfill_order(session)
+        line_items = session.line_items
+        # Fulfill the purchase...
+        fulfill_order(line_items)
 
     # Passed signature verification
     return HttpResponse(status=200)
 
 
-def fulfill_order(session):
+def fulfill_order(line_items):
     # TODO: fill me in
-    order_id = int(session.metadata.order_id)
+    object_id = int(line_items.metadata.order_id)
     print("Fulfilling order")
